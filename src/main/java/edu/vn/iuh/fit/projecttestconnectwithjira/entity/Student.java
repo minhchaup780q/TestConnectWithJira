a8ebb8f7c8ad2acb2361e9b73fcfd2ebf298623b
hj3j <minhchaup780q@gmail.com>
2026-07-25 14:32:00 +0700

    SCRUM-1: test link github repo to jira

---
+++ src/main/java/edu/vn/iuh/fit/projecttestconnectwithjira/entity/Student.java
@@ -0,0 +1,9 @@
+package edu.vn.iuh.fit.projecttestconnectwithjira.entity;
+
+public class Student {
+    private String mssv;
+
+    public Student(String mssv) {
+        this.mssv = mssv;
+    }
+}
---
+++ src/main/java/edu/vn/iuh/fit/projecttestconnectwithjira/entity/Teacher.java
@@ -0,0 +1,9 @@
+package edu.vn.iuh.fit.projecttestconnectwithjira.entity;
+
+public class Teacher {
+    private String msgv;
+
+    public Teacher(String msgv) {
+        this.msgv = msgv;
+    }
+}
